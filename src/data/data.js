5ec2f8aef108f3aab17340082600bba3e0c60629
irannagkk <hejjegariranna@gmail.com>
2024-02-20 20:54:41 +0530

    portfolio project added

---
+++ src/data/data.js
@@ -0,0 +1,27 @@
+import WorkIm from '../assets/projects/workImg.jpeg'
+import RealEst from '../assets/projects/realestate.jpg'
+
+export const data=[
+    {
+        id:1,
+        name:"Real Estate Application",
+        image:RealEst,
+        github:"",
+        live:"https://real-estate-v01s.onrender.com/",
+    },
+    // {
+    //     id:2,
+    //     name:"React JS Application",
+    //     image:RealEst,
+    //     github:"",
+    //     live:"",
+    // },
+    // {
+    //     id:3,
+    //     name:"React JS Application",
+    //     image:WorkIm,
+    //     github:"",
+    //     live:"",
+    // },
+   
+]
